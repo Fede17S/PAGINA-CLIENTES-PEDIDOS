711def2af11056b5a30d40315f16d810162c30ca
Fede17S <124637013+Fede17S@users.noreply.github.com>
2026-07-30 23:23:03 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // DISTRIBIX Service Worker v1.5
-const CACHE_NAME = 'distribix-v1.6';
-const CACHE_STATIC = 'distribix-static-v1.6';
+const CACHE_NAME = 'distribix-v1.9';
+const CACHE_STATIC = 'distribix-static-v1.9';
 
 // Se conservan los nombres usados en GitHub. PANEL-index_15.html permite probar
 // directamente el archivo de desarrollo; si no existe en producción no bloquea
